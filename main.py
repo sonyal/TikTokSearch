bbdf3851a5ffbf41f8798270fecbb6f7f2a8272b
sonyal <slew@umd.edu>
2020-09-12 11:36:01 -0400

    pseudocode for program

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+import mechanize
+
+#upload TikTok data 
+#enter search terms
+
+#open data file
+#parse file
+#collect list of matches
+
+#display clickable list of thumbnails for matches
